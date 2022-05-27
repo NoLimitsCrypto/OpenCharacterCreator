911ed65bf2871570764ccc3922d9e3e929893c0b
xpertWebApp <appweb.live@gmail.com>
2022-05-27 17:18:19 +0530

    port changed

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     // https:true,
     hmr: {
       host: "avatars.darknexus.city",
-      protocol: "ws",
+      protocol: "wss",
       clientPort: 443,
       
     },

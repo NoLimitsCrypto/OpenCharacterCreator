76bcb9833d21f3b78ddc997081607679060ef259
xpertWebApp <appweb.live@gmail.com>
2022-05-27 17:06:39 +0530

    port changed

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   plugins: [reactRefresh()],
   clearScreen: false,
   server: {
-    host: true,
+    host: "0.0.0.0",
     port: 3000,
     strictPort: true,
     https:true,

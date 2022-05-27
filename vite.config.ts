5ac37fa121cd409afe67503217157b72e60d3923
xpertWebApp <appweb.live@gmail.com>
2022-05-27 14:09:21 +0530

    port changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,22 @@ import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    // host: '0.0.0.0',
-    // https: true,
-    hmr: {
-      host: 'avatars.darknexus.city',
-      port: 443,
-      // protocol: 'wss',
+  mode: "development",
+  resolve: {
+    alias: {
+      _: path.resolve(__dirname, "src"),
     },
   },
-  resolve: {
-  },
   plugins: [reactRefresh()],
-  define: {
-    "global": {},
+  clearScreen: false,
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    strictPort: true,
+    hmr: {
+      host: "avatars.darknexus.city",
+      port: 443,
+      protocol: "https",
+    },
   },
 });

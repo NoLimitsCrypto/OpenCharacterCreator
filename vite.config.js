80e939c4d9ec4189dde6c7080d79789866a5852d
Hayk <55106546+mavisakalyan@users.noreply.github.com>
2022-06-06 17:47:45 +0400

    dist

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import * as path from "path";
+// https://vitejs.dev/config/
+export default defineConfig({
+    mode: "development",
+    resolve: {
+        alias: {
+            _: path.resolve(__dirname, "src"),
+        },
+    },
+    plugins: [reactRefresh()],
+    clearScreen: false,
+    server: {
+        host: "0.0.0.0",
+        port: 3000,
+        strictPort: true,
+        hmr: {
+            port: 3001,
+            protocol: "ws",
+        },
+    },
+});

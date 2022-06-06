d2152d1715b5ecc33323f07e0188148f8e436124
Hayk <55106546+mavisakalyan@users.noreply.github.com>
2022-06-06 18:00:18 +0400

    Update

--- src/components/Editor/search.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import SearchIcon from "@mui/icons-material/Search";
-import { useGlobalState } from "../AuthContextWrap";
-export default function SearchElements() {
-    const { search, setSearch } = useGlobalState();
-    return (_jsxs("div", { children: [_jsx(SearchIcon, { className: "search-icon" }), _jsx("input", { type: "text", placeholder: "Search", value: search, onChange: (e) => setSearch(e.target.value) })] }));
-}

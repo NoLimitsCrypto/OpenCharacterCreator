cb5eee4e41d4279fa5b880faeac3f82e5287a478
Hayk <55106546+mavisakalyan@users.noreply.github.com>
2022-06-06 16:26:00 +0400

    Vite build issue fixed.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
-import * as path from "path";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  esbuild:{
-    target:"es2018"
-  }
+    plugins: [reactRefresh()],
+    esbuild: {
+        target: "es2018"
+    }
 });
